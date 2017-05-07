e80268a60834522d470827d884c36d593f720755
Viv-Iam <vivian.opondoh@gmail.com>
2017-05-07 19:53:09 +0300

    Add private name and email properties in Person.java file

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,5 +1,9 @@
 public class Person {
+  private String name;
+  private String email;
 
-  public Person() {
+  public Person(String name, String email) {
+    this.name = name;
+    this.email = email;
   }
 }
