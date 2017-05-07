b719781947c2a9b2a10a31166159bf0d37b722a8
Viv-Iam <vivian.opondoh@gmail.com>
2017-05-07 20:44:23 +0300

    Add content to DatabaseRule.java

--- src/test/java/PersonTest.java
+++ src/test/java/PersonTest.java
@@ -3,6 +3,8 @@ import static org.junit.Assert.*;
 import org.sql2o.*;
 
 public class PersonTest {
+  @Rule
+  public DatabaseRule database = new DatabaseRule();
 
   @Test
   public void person_instantiatesCorrectly_true() {
