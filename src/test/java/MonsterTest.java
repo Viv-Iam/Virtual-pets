25812d73ec50e6a3b1bba58c64a53f39e62b8b15
Viv-Iam <vivian.opondoh@gmail.com>
2017-05-08 15:58:21 +0300

    Add test to ensure the exception gets thrown

--- src/test/java/MonsterTest.java
+++ src/test/java/MonsterTest.java
@@ -177,4 +177,12 @@ public class MonsterTest {
     assertTrue(testMonster.getPlayLevel() <= Monster.MAX_PLAY_LEVEL);
   }
 
+  @Test(expected = UnsupportedOperationException.class)
+  public void sleep_throwsExceptionIfSleepLevelIsAtMaxValue(){
+    Monster testMonster = new Monster("Bubbles", 1);
+    for(int i = Monster.MIN_ALL_LEVELS; i <= (Monster.MAX_SLEEP_LEVEL); i++){
+      testMonster.sleep();
+    }
+  }
+
 }
