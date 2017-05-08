b19536a526f6eada2cddf5a637abd6cd01d7e1af
Viv-Iam <vivian.opondoh@gmail.com>
2017-05-08 11:09:58 +0300

    Add test that retrieves all monsters from data base

--- src/test/java/MonsterTest.java
+++ src/test/java/MonsterTest.java
@@ -1,6 +1,7 @@
 import org.junit.*;
 import static org.junit.Assert.*;
 import org.sql2o.*;
+import java.util.Arrays;
 
 public class MonsterTest {
 
@@ -74,6 +75,18 @@ public class MonsterTest {
     testMonster.save();
     Monster savedMonster = Monster.find(testMonster.getId());
     assertEquals(savedMonster.getPersonId(), testPerson.getId());
+  }
+
+  @Test
+  public void getMonsters_retrievesAllMonstersFromDatabase_monstersList() {
+    Person testPerson = new Person("Henry", "henry@henry.com");
+    testPerson.save();
+    Monster firstMonster = new Monster("Bubbles", testPerson.getId());
+    firstMonster.save();
+    Monster secondMonster = new Monster("Spud", testPerson.getId());
+    secondMonster.save();
+    Monster[] monsters = new Monster[] { firstMonster, secondMonster };
+    assertTrue(testPerson.getMonsters().containsAll(Arrays.asList(monsters)));
   }
 
 }
