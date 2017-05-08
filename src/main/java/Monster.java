e09020ba10cb81d26d8d0bcbd446b2c526ea4152
Viv-Iam <vivian.opondoh@gmail.com>
2017-05-08 07:42:07 +0300

    Add logic to implement getPersonId() method

--- src/main/java/Monster.java
+++ src/main/java/Monster.java
@@ -1,12 +1,17 @@
 public class Monster {
+  private String name;
+  private int personId;
 
   public Monster(String name, int personId) {
-
     this.name = name;
+    this.personId = personId;
   }
 
 public String getName() {
   return name;
 }
 
+publlic int getPersonId() {
+  return personId;
+}
 }
