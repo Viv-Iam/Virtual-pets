c1ede5801d30a9cd4c8d0128aff6ad0e70f7c53f
Viv-Iam <vivian.opondoh@gmail.com>
2017-05-08 15:21:17 +0300

    Add test that confirms exception is received if user attempts to raise monster's foodLevel beyond MAX_FOOD_LEVEL

--- src/test/java/MonsterTest.java
+++ src/test/java/MonsterTest.java
@@ -149,4 +149,12 @@ public class MonsterTest {
     assertTrue(testMonster.getFoodLevel() <= Monster.MAX_FOOD_LEVEL);
   }
 
+  @Test(expected = UnsupportedOperationException.class)
+   public void feed_throwsExceptionIfFoodLevelIsAtMaxValue(){
+     Monster testMonster = new Monster("Bubbles", 1);
+     for(int i = Monster.MIN_ALL_LEVELS; i <= (Monster.MAX_FOOD_LEVEL); i++){
+       testMonster.feed();
+     }
+   }
+
 }
