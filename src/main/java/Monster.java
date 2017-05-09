b7347107158c70813bc35e7aff1879eb8a5db603
Viv-Iam <vivian.opondoh@gmail.com>
2017-05-09 14:45:38 +0300

    Declare Monster class an abstract class

--- src/main/java/Monster.java
+++ src/main/java/Monster.java
@@ -2,31 +2,35 @@ import org.sql2o.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.sql.Timestamp;
+import java.util.Timer;
+import java.util.TimerTask;
 
-public class Monster {
-  private String name;
-  private int personId;
-  private int id;
-  private int foodLevel;
-  private int sleepLevel;
-  private int playLevel;
-  private Timestamp birthday;
-  private Timestamp lastSlept;
-  private Timestamp lastAte;
-  private Timestamp lastPlayed;
+public abstract class Monster {
+  public String name;
+  public int personId;
+  public int id;
+  public int foodLevel;
+  public int sleepLevel;
+  public int playLevel;
+  public Timestamp birthday;
+  public Timestamp lastSlept;
+  public Timestamp lastAte;
+  public Timestamp lastPlayed;
+  public Timer timer;
 
   public static final int MAX_FOOD_LEVEL = 3;
   public static final int MAX_SLEEP_LEVEL = 8;
   public static final int MAX_PLAY_LEVEL = 12;
   public static final int MIN_ALL_LEVELS = 0;
 
-  public Monster(String name, int personId) {
-    this.name = name;
-    this.personId = personId;
-    playLevel = MAX_PLAY_LEVEL / 2;
-    sleepLevel = MAX_SLEEP_LEVEL / 2;
-    foodLevel = MAX_FOOD_LEVEL / 2;
-  }
+  // public Monster(String name, int personId) {
+  //   this.name = name;
+  //   this.personId = personId;
+  //   playLevel = MAX_PLAY_LEVEL / 2;
+  //   sleepLevel = MAX_SLEEP_LEVEL / 2;
+  //   foodLevel = MAX_FOOD_LEVEL / 2;
+  //   timer = new Timer();
+  // }
 
 public String getName() {
   return name;
@@ -55,6 +59,18 @@ public int getPlayLevel(){
     public Timestamp getBirthday(){
     return birthday;
   }
+
+  public Timestamp getLastSlept(){
+   return lastSlept;
+ }
+
+ public Timestamp getLastAte(){
+    return lastAte;
+  }
+
+  public Timestamp getLastPlayed(){
+   return lastPlayed;
+ }
 
 public static List<Monster> all() {
     String sql = "SELECT * FROM monsters";
@@ -103,11 +119,12 @@ public static List<Monster> all() {
    }
    return true;
  }
-
  public void depleteLevels(){
-    playLevel--;
-    foodLevel--;
-    sleepLevel--;
+    if (isAlive()){
+      playLevel--;
+      foodLevel--;
+      sleepLevel--;
+    }
   }
 
   // public void play(){
@@ -123,23 +140,55 @@ public static List<Monster> all() {
   // }
 
   public void feed(){
-    if (foodLevel >= MAX_FOOD_LEVEL){
-      throw new UnsupportedOperationException("You cannot feed your monster anymore!");
+      if (foodLevel >= MAX_FOOD_LEVEL){
+        throw new UnsupportedOperationException("You cannot feed your monster anymore!");
+      }
+      try(Connection con = DB.sql2o.open()) {
+        String sql = "UPDATE monsters SET lastate = now() WHERE id = :id";
+        con.createQuery(sql)
+          .addParameter("id", id)
+          .executeUpdate();
+        }
+      foodLevel++;
     }
-    foodLevel++;
-  }
 
-  public void play(){
+    public void play(){
       if (playLevel >= MAX_PLAY_LEVEL){
         throw new UnsupportedOperationException("You cannot play with monster anymore!");
       }
+      try(Connection con = DB.sql2o.open()) {
+        String sql = "UPDATE monsters SET lastplayed = now() WHERE id = :id";
+        con.createQuery(sql)
+          .addParameter("id", id)
+          .executeUpdate();
+        }
       playLevel++;
     }
     public void sleep(){
-        if (sleepLevel >= MAX_SLEEP_LEVEL){
-          throw new UnsupportedOperationException("You cannot make your monster sleep anymore!");
-        }
-        sleepLevel++;
-      }
+     if (sleepLevel >= MAX_SLEEP_LEVEL){
+       throw new UnsupportedOperationException("You cannot make your monster sleep anymore!");
+     }
+     try(Connection con = DB.sql2o.open()) {
+       String sql = "UPDATE monsters SET lastslept = now() WHERE id = :id";
+       con.createQuery(sql)
+         .addParameter("id", id)
+         .executeUpdate();
+       }
+     sleepLevel++;
+   }
+
+   public void startTimer(){
+       Monster currentMonster = this;
+       TimerTask timerTask = new TimerTask(){
+         @Override
+         public void run() {
+           if (currentMonster.isAlive() == false){
+             cancel();
+           }
+           depleteLevels();
+         }
+       };
+       this.timer.schedule(timerTask, 0, 600);
+     }
 
 }
