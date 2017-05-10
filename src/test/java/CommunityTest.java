500c0cda1e89dd288a87a1f9bebe5ee2a896682c
Viv-Iam <vivian.opondoh@gmail.com>
2017-05-10 16:32:54 +0300

    Add test ensure community class instantiates with new Community Objects

--- src/test/java/CommunityTest.java
+++ src/test/java/CommunityTest.java
@@ -8,4 +8,10 @@ public class CommunityTest {
   @Rule
   public DatabaseRule database = new DatabaseRule();
 
+  @Test
+    public void community_instantiatesCorrectly_true() {
+      Community testCommunity = new Community("Water Enthusiasts", "Lovers of all things water monsters!");
+      assertEquals(true, testCommunity instanceof Community);
+    }
+
 }
